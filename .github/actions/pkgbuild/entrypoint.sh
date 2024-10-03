e127bfe598afc1c804fec2f62777b7fa5f21bd54
Nathan <ndowens@artixlinux.org>
2024-10-03 00:08:28 -0500

    --amend

--- .github/actions/pkgbuild/entrypoint.sh
+++ .github/actions/pkgbuild/entrypoint.sh
@@ -58,9 +58,9 @@ if [ -n "${INPUT_MAKEPKGCONF:-}" ]; then
     fi
 fi
 
-for i in cachyos-mirrorlist mirrorlist-chaotic cachyos-v3-mirrorlist ; do
-	cp -v $i /etc/pacman.d
-done
+#for i in cachyos-mirrorlist mirrorlist-chaotic cachyos-v3-mirrorlist ; do
+#	cp -v $i /etc/pacman.d
+#done
 $(pwd)
 # Update before continuing
 pacman -Syu --noconfirm
